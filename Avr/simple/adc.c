a4e6066881d4c68c0c4f0372ba2e2ac8e95d67c9
Anders Kortegaard <ank@askapps.eu>
2016-01-17 16:36:47 +0100

    adc light sensor example

---
+++ Avr/simple/adc.c
@@ -0,0 +1,29 @@
+/*
+*         Title : ADC - light sensor
+*        Author : Anders S. Kortegaard
+*          Date : 17/01 - 2016
+*
+*   Description : Reading analog value, using adc
+*/
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../lib/adc.h"   //importing adc lib 
+
+int main(){
+  //initializing adc
+  adc_init();
+  //Setting input outputs (input on pin 0, light sensor)
+  DDRA = 0xFE;
+  while(1)
+    {
+      //Checkking adc/light value, and set pin high if value is over 400
+      if (adc_read(0) > 400) {
+        PORTA |= 1<<PA7;
+      }else{ // if not setting pin low
+        PORTA &= 0<<PA7;
+      }
+      _delay_ms(50);
+    }
+  return 0;
+}
