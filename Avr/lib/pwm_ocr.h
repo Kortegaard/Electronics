3523ee2297fad1eb080ac70c552a1f135238d955
Anders Kortegaard <ank@askapps.eu>
2016-01-18 21:19:29 +0100

    add pwm

---
+++ Avr/lib/pwm_ocr.h
@@ -0,0 +1,24 @@
+#ifndef __pwm_h__
+  #define __pwm_h__
+  void pwm_init()
+  {
+   //TCCR0 - Timer Counter Control Register (TIMER0)
+
+
+   TCCR0|=(1<<WGM00)|(1<<WGM01)|(1<<COM01)|(1<<CS00);
+
+   //Set OC0 PIN as output. It is  PB3 on ATmega16 ATmega32
+   DDRB|=(1<<PB3);
+  }
+
+
+  void pwm_set(uint8_t duty)
+  {
+    OCR0=duty;
+  }
+
+  void pwm_wait()
+  {
+    _delay_loop_2(3200);
+  }
+#endif
---
+++ Avr/simple/pwm.c
@@ -0,0 +1,36 @@
+/*
+*         Title : PWM
+*        Author : Anders S. Kortegaard
+*          Date : 17/01 - 2016
+*
+*   Description : PWM
+*       Credits : http://www.avrfreaks.net/forum/tut-c-newbies-guide-avr-pwm-incomplete?page=all
+*                 http://extremeelectronics.co.in/avr-tutorials/pwm-signal-generation-by-using-avr-timers-part-ii/
+*/
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../lib/pwm_ocr.h"
+
+int i = 0;
+
+void main()
+{
+   uint8_t brightness=0;
+
+   //Initialize PWM
+   pwm_init();
+
+   while(1){
+     for(i = 0 ; i < 255; i++){
+       //Setting pwm value
+       pwm_set(i);
+       pwm_wait();
+     }
+     for(i = 255 ; i > 0; i--){
+       pwm_set(i);
+       pwm_wait();
+     }
+   }
+}
